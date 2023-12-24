f438785a9d175bb77768a9a05aa06106ca3f9ebf
Wang Shaoyu <wsysuper@gmail.com>
2023-12-24 23:20:43 +0900

    output 2023-12-24

---
+++ shared-materials/hello-world-examples/html/logic.js
@@ -0,0 +1,8 @@
+const buttonA = document.querySelector('#button_A')
+const headingA = document.querySelector('#heading_A')
+
+buttonA.onclick = () => {
+  const name = prompt('あなたの名前は何ですか？')
+  alert(`こんにちは、${name}さん、はじめまして！`)
+  headingA.textContent = `${name}さん、ようこそ`
+}
